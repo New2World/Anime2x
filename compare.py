4d5550fae02c554ee12604abec2fd123b7131d6a
Ritchie <ritchie_w@outlook.com>
2019-07-23 21:01:37 -0400

    add PSNR comparation

---
+++ compare.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+
+origin = cv2.imread("sakura.png", 0)
+test = cv2.imread("sakura_half.png")
+output = cv2.imread("output.png", 0)
+target = cv2.imread("target.png", 0)
+
+x2_origin = cv2.resize(test, (636,1000), interpolation=cv2.INTER_CUBIC)
+x2_origin = cv2.cvtColor(x2_origin, cv2.COLOR_BGR2GRAY)
+
+def PSNR(inp, tar):
+    peak = inp.max()
+    mse = np.sum((inp-tar)**2)/inp.size
+    return 10 * (2 * np.log10(peak) - np.log10(mse))
+
+print(f"model output: {PSNR(origin, output)}")
+print(f"cubic output: {PSNR(origin, x2_origin)}")
+print(f"target output: {PSNR(target, origin)}")
