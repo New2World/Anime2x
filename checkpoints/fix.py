40707ea136e3eb5d676d57cd0ce25cdc8e79bb6c
Ritchie <ritchie_w@outlook.com>
2019-08-01 07:07:27 -0400

    add checkpoints

---
+++ checkpoints/fix.py
@@ -0,0 +1,13 @@
+import os
+import torch
+
+fl = list(os.walk('.'))[0][2]
+fl = [f for f in fl if f.endswith('.pt')]
+
+for f in fl:
+    n = int(f.split('-')[1])
+    s = torch.load(f)
+    # s['scheduler_state_dict']['last_epoch'] = n
+    # s['scheduler_state_dict']['step_size'] = 50
+    # torch.save(s, f)
+    print(f'Ep.{n} - {s["scheduler_state_dict"]} - {[params["lr"] for params in s["optimizer_state_dict"]["param_groups"]]}')
