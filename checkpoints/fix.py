f8fc6df26b11c07b06a2692a040d30fa8a5df86c
Ritchie <ritchie_w@outlook.com>
2019-08-01 11:37:17 -0400

    step size 40

--- checkpoints/fix.py
+++ checkpoints/fix.py
@@ -8,6 +8,6 @@ for f in fl:
     n = int(f.split('-')[1])
     s = torch.load(f)
     # s['scheduler_state_dict']['last_epoch'] = n
-    # s['scheduler_state_dict']['step_size'] = 50
-    # torch.save(s, f)
+    s['scheduler_state_dict']['step_size'] = 40
+    torch.save(s, f)
     print(f'Ep.{n} - {s["scheduler_state_dict"]} - {[params["lr"] for params in s["optimizer_state_dict"]["param_groups"]]}')
